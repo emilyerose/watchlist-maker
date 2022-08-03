99e675c4c3dc8169343962a89910bbe61ea7fd76
emilyerose <emilyrose0021@gmail.com>
2022-08-03 14:28:39 -0400

    updated script.js pages to change location

--- JS/script.js
+++ JS/script.js
@@ -1,65 +1,9 @@
-omdbAPIkey = '1260ba33';
-watchmodeAPIkey = 'h0vFF0GYi3hvZkzF4vw5LphfH6Nx2LfrwlxaFQXw'
+var watchmodeAPIkey = 'h0vFF0GYi3hvZkzF4vw5LphfH6Nx2LfrwlxaFQXw';
+var searchform = document.querySelector('form');
 
-
-function getFilm(title) {
-    omdbquery = 'https://www.omdbapi.com/?apikey=' + omdbAPIkey +'&t=' + title;
-    let postersrc;
-    let title;
-    let runtime;
-    let posterlink;
-    let plot;
-    let subscription = [];
-    fetch(omdbquery)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        posterlink = data.Poster;
-        console.log(data)
-        let rottentomatoes;
-        if (data.Ratings[1]){
-            rottentomatoes = data.Ratings[1].Value
-        }
-        
-        let id = data.imdbID;
-        runtime = parseFloat(data.Runtime);
-        title = data.Title;
-        plot = data.Plot;
-        return id
-    })
-    .then(function(imdbID) {
-        watchmodequery = `https://api.watchmode.com/v1/title/${imdbID}/sources/?apiKey=${watchmodeAPIkey}&regions=US`
-        //watchmodequery = 'https://api.watchmode.com/v1/sources/?regions=US&apiKey=' + watchmodeAPIkey
-        fetch(watchmodequery)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-           console.log(data)
-           let subscription = []
-           for (source in data) {
-            if (source.type==="sub") {
-                subscription.push(source.name)
-            }
-           }
-           
-           function addToWatchlist(filmID) {
-            let obj = {
-                title: data.Title,
-                runtime: parseFloat(data.Runtime),
-                postersrc: posterlink, 
-                sources: subscription,
-                plot: plot,
-            }
-            localStorage.setItem('id',JSON.stringify(obj));
-            
-        }
-           
-        })
-    })
-}
-
-getFilm('black swan')
-
-
+searchform.addEventListener('submit', (event) => {
+    event.preventDefault();
+    searchentry = document.querySelector('input');
+    let film = (searchentry.value);
+    window.location.assign('./rating-page/index.html?f=' + film);
+})
