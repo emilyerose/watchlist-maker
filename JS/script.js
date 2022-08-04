926124f9cc989c619678374e347a39004ac979a0
emilyerose <emilyrose0021@gmail.com>
2022-08-04 12:48:31 -0400

    dynamically rendering watchlist

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,149 @@
 var watchmodeAPIkey = 'h0vFF0GYi3hvZkzF4vw5LphfH6Nx2LfrwlxaFQXw';
+var omdbAPIkey = '1260ba33';
 var searchform = document.querySelector('form');
+var filmid;
+
+
+
+
+
 
 searchform.addEventListener('submit', (event) => {
     event.preventDefault();
     searchentry = document.querySelector('input');
     let film = (searchentry.value);
-    window.location.assign('./rating-page/index.html?f=' + film);
+    getFilm(film)
+    //window.location.assign('./rating-page/index.html?f=' + film)
 })
 
+document.addEventListener('click', (event) => {
+    if (event.target.matches('remove-wl')){
+        let id = event.target.id
+        localStorage.removeItem(id)
+    }
+
+    //window.location.reload()
+})
+
+
+async function getFilm(title) {
+    omdbquery = 'https://www.omdbapi.com/?apikey=' + omdbAPIkey +'&t=' + title;
+    let formaltitle;
+    let runtime;
+    let posterlink;
+    let plot;
+    fetch(omdbquery)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        let ratings = data.Ratings;    
+        filmid = data.imdbID;
+        runtime = parseFloat(data.Runtime);
+        formaltitle = data.Title;
+        plot = data.Plot;
+        posterlink = data.Poster;
+        return filmid
+    })
+    .then(function(imdbID) {
+        watchmodequery = `https://api.watchmode.com/v1/title/${imdbID}/sources/?apiKey=${watchmodeAPIkey}&regions=US`
+
+        fetch(watchmodequery)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+           console.log(data)
+           let subscription = [];
+           for (let n =0; n<data.length; n++) {
+            let source = data[n];
+            if (source.type==="sub") {
+                subscription.push(source.name)
+            }
+           }
+           
+           
+            let obj = {
+                title: formaltitle,
+                id: filmid,
+                runtime: parseFloat(runtime),
+                postersrc: posterlink, 
+                sources: subscription,
+                plot: plot,
+            }
+            localStorage.setItem(filmid,JSON.stringify(obj));   
+           
+        })
+    })
+}
+
+populatePage();
+
  function populatePage() {
-    for (const key in localStorage) {
-        let obj = localStorage.getItem(key);
+    if (localStorage.length===0) {
+        return
+    }
+    else {
+    for (let x =0; x<localStorage.length; x++) {
+        let key = localStorage.key(x);
+        let obj = JSON.parse(localStorage.getItem(key));
+        console.log(obj);
         let title = obj.title;
         let runtime = obj.runtime;
         let posterlink = obj.postersrc;
         let plot = obj.plot;
-        let sourcesArr = obj.sources;
+        let sourcesArr = obj.sources.join(', ');
+            var container = document.querySelector('.movie-cards');
+            var card = document.createElement("div");
+            card.classList.add('card','column');
+            card.innerHTML = `
+            <div class="dropdown is-hoverable">
+            <div class="dropdown-trigger">
+              <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+                <span><i class="fa-solid fa-ellipsis"></i></span>
+              </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+              <div class="remove-wl dropdown-content" id = ${obj.id}>
+                <div class="dropdown-item">
+                  <p class="removeBtn"> Remove from watchlist </p>
+                </div>
+              </div>
+            </div>
+           </div>
+           
+           <div class="card-image">
+            <figure>
+              <img
+                src=${posterlink}
+                alt="${title} image"
+              />
+            </figure>
+           </div>
+           <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">${title}</p>
+                <h3 class="media-left streaming">Available to stream via: ${sourcesArr}</h3>
+                <div class="streaming-services"></div>
+              </div>
+            </div>
+            <div class="content">
+              <p>${plot}</p>
+              <br />
+            </div>
+           </div>`
         
-        //get sources from watchmode later, for now, just grab them from local storage
+
+           container.appendChild(card);
+        }
+        
     }
+
+
  }
+
+
+
+ 
