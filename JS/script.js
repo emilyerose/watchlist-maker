7cd1a1086cea8627bc599ae79c54f5b0e2c6b462
emilyerose <emilyrose0021@gmail.com>
2022-08-04 10:55:24 -0400

    pushing for safety

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,16 @@ searchform.addEventListener('submit', (event) => {
     let film = (searchentry.value);
     window.location.assign('./rating-page/index.html?f=' + film);
 })
+
+ function populatePage() {
+    for (const key in localStorage) {
+        let obj = localStorage.getItem(key);
+        let title = obj.title;
+        let runtime = obj.runtime;
+        let posterlink = obj.postersrc;
+        let plot = obj.plot;
+        let sourcesArr = obj.sources;
+        
+        //get sources from watchmode later, for now, just grab them from local storage
+    }
+ }
